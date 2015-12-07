d87bcbac7a0cdc6b3e4c5871e3a6d1c53883dcf7
zephye030 <zephyr0_0@126.com>
2015-12-07 15:34:18 +0800

    SMS接口工具

---
+++ src/main/java/com/service/BBPayApiService.java
@@ -0,0 +1,100 @@
+package com.service;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Administrator on 2015/12/7.
+ */
+@Service
+public class BBPayApiService extends BaseService {
+    //获取配置文件payapi/properties
+    private ResourceBundle resb = ResourceBundle.getBundle("payapi");
+    //币币支付请求地址
+    private String payUrl = resb.getString("payapi.payUrl");
+    //币币退款请求地址
+    private String payOutUrl = resb.getString("payapi.payOutUrl");
+    //币币对账单下载请求地址
+    private String fundsSettle = resb.getString("payapi.fundsSettle");
+
+    // 商户账户编号
+    private String merchantaccount = resb.getString("payapi.merchantaccount");
+    // 从配置文件读取币币的公钥
+    private String bbPublicKey = resb.getString("payapi.bb_publickey");
+
+    // 从配置文件读取商户自己的私钥
+    private String merchantPrivateKey = resb.getString("payapi.merchant_privatekey");
+
+    /* 生成订单orderId */
+    protected static final String getOrderSql = "EXEC [Wit_BaseInfo].[dbo].[dc_getOrderID] ?,?,?";
+
+    /**
+     * 给MAP中加订单ID
+     * @param paramMap 需要加订单ID的MAP
+     * @param remark 订单备注
+     */
+    @Transactional(propagation= Propagation.REQUIRES_NEW)
+    public void putOrderID(Map<String, String> paramMap, String remark) {
+        long orderID = getOrderID(paramMap.get("service")==null?" ":paramMap.get("service"), JSON.toJSONString(paramMap), remark);
+        paramMap.put("orderNo", String.valueOf(orderID));
+    }
+
+    /**
+     * 获得订单ID 按照年月日和当天已下订单次数生成订单
+     * @param interfaceName 调用的接口名
+     * @param params		调用的所有参数 JSON格式
+     * @param remark		备注 可空
+     * @return 订单号
+     */
+    @Transactional(propagation=Propagation.REQUIRES_NEW)
+    public long getOrderID(String interfaceName,String params,String remark) {
+        return jdbcTemplate.queryForLong(getOrderSql, interfaceName,params,remark);
+    }
+
+    /**
+     * 修改订单返回状态
+     * @param orderid 订单ID
+     * @param returninfo 返回信息
+     */
+    private void updateOrderRetrunState(long orderid,String returninfo){
+        StringBuffer sql = new StringBuffer();
+        sql.append("update [Wit_BaseInfo].[dbo].[wit_yjf_detail] ");
+        sql.append("set state = 1 , returninfo = ? ");
+        sql.append("where orderID = ? ");
+        jdbcTemplate.update(sql.toString(), returninfo , orderid);
+    }
+
+    /**
+     * 获得请求易极付的基础参数
+     * @param service 接口名称
+     * @param orderNo 订单号
+     */
+    public static Map<String, String> getBaseParam(String service,String orderNo){
+        Map<String, String> paramMap = new HashMap<String, String>();
+        paramMap.put("service", service);
+//        paramMap.put("partnerId", YjfConfig.partnerId);
+//        paramMap.put("orderNo", orderNo);
+//        paramMap.put("signType", YjfConfig.signType.getName());
+//        paramMap.put("inputCharset", YjfConfig.inputCharset);
+        return paramMap;
+    }
+
+    /**
+     * 获得请求易极付的基础参数
+     * @param service 接口名称
+     */
+    public static Map<String, String> getBaseParam(String service){
+        Map<String, String> paramMap = new HashMap<String, String>();
+        paramMap.put("service", service);
+//        paramMap.put("partnerId", YjfConfig.partnerId);
+//        paramMap.put("signType", YjfConfig.signType.getName());
+//        paramMap.put("inputCharset", YjfConfig.inputCharset);
+        return paramMap;
+    }
+}
---
+++ src/main/java/com/utils/sms/SMSUtiles.java
@@ -0,0 +1,85 @@
+package com.utils.sms;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Administrator on 2015/12/7.
+ */
+public class SMSUtiles {
+    //获取配置文件sms.properties
+    private static ResourceBundle resb = ResourceBundle.getBundle("sms");
+    //短信地址
+    private static String Url = resb.getString("sms.url");
+    //短信登陆名
+    private static String account = resb.getString("sms.account");
+    //短信密码
+    private static String password = resb.getString("sms.password");
+
+    public static void sendSMS(String phone, String content) {
+        HttpClient client = new HttpClient();
+        PostMethod method = new PostMethod(Url);
+
+        //client.getParams().setContentCharset("GBK");
+        client.getParams().setContentCharset("UTF-8");
+        method.setRequestHeader("ContentType","application/x-www-form-urlencoded;charset=UTF-8");
+
+
+        int mobile_code = (int)((Math.random()*9+1)*100000);
+
+        NameValuePair[] data = {//提交短信
+                new NameValuePair("account", account),
+                new NameValuePair("password", password), //密码可以使用明文密码或使用32位MD5加密
+                //new NameValuePair("password", util.StringUtil.MD5Encode("密码")),
+                new NameValuePair("mobile", phone),
+                new NameValuePair("content", content),
+        };
+
+        method.setRequestBody(data);
+
+
+        try {
+            client.executeMethod(method);
+
+            String SubmitResult =method.getResponseBodyAsString();
+
+            //System.out.println(SubmitResult);
+
+            Document doc = DocumentHelper.parseText(SubmitResult);
+            Element root = doc.getRootElement();
+
+
+            String code = root.elementText("code");
+            String msg = root.elementText("msg");
+            String smsid = root.elementText("smsid");
+
+
+            System.out.println(code);
+            System.out.println(msg);
+            System.out.println(smsid);
+
+            if(code == "2"){
+                System.out.println("短信提交成功");
+            }
+
+        } catch (HttpException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
