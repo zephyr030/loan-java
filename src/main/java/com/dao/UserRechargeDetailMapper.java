2043c16f7b1b6181a141edbee9a2474a193e9f6c
zhouwei1985 <251556768@qq.com>
2015-12-08 15:51:10 +0800

    测试分页插件

--- src/main/java/com/dao/UserRechargeDetailMapper.java
+++ src/main/java/com/dao/UserRechargeDetailMapper.java
@@ -2,9 +2,15 @@ package com.dao;
 
 import com.dao.common.BaseMapper;
 import com.dao.common.MyBatisMapper;
+import com.dao.util.Searchable;
 import com.model.UserRechargeDetail;
+
+import java.util.List;
+import java.util.Map;
 
 @MyBatisMapper
 public interface UserRechargeDetailMapper extends BaseMapper<UserRechargeDetail, Long>{
 
+    //充值列表
+    List<Map<String,Object>> rechargeList(Searchable searchable);
 }
